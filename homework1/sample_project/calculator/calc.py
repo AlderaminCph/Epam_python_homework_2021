56dbbea58d9367962150d57583e0b1609ba9bd80
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-22 17:26:21 +0300

    task1 -- bugs in the sample_project code were found and fixed and extra test cases were written

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -1,2 +1,12 @@
+'''task1
+    1 please create your own git repository on github.
+    2 (optional) setup pre-commit hook with black and isort formatting for the repo
+    3 initialize .gitignore in the repository root (you can use this sample)
+    4 create a homework1 directory in the repo
+    5 then copy the sample_project into the directory.
+    6 fix all bugs in the sample_project code
+    7 write an extra test for each found bug
+
+'''
 def check_power_of_2(a: int) -> bool:
-    return not (bool(a & (a - 1)))
+    return not (bool(a & (a - 1))) and a != 0
--- homework1/sample_project/tests/test_calc.py
+++ homework1/sample_project/tests/test_calc.py
@@ -2,10 +2,19 @@ import pytest
 
 from homework1.sample_project.calculator.calc import check_power_of_2
 
-def test_positive_case():
-    """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
-    
-def test_negative_case():
-    """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)    
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        (65536, True),
+        (12, False),
+        (1, True),
+        (0, False),
+        
+    ],
+)
+def test_power_of_2(value: int, expected_result: bool):
+    actual_result = check_power_of_2(value)
+    if actual_result != expected_result:
+    	print(value) 
+    assert actual_result == expected_result
