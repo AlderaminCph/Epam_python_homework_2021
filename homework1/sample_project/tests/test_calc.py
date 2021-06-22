b8ed993f3afa21ce7d2754a6bb44422cc1b09e8e
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-23 00:15:18 +0300

    test_calc.py was edited with pep8

--- homework1/sample_project/tests/test_calc.py
+++ homework1/sample_project/tests/test_calc.py
@@ -10,11 +10,10 @@ from homework1.sample_project.calculator.calc import check_power_of_2
         (12, False),
         (1, True),
         (0, False),
-        
     ],
 )
 def test_power_of_2(value: int, expected_result: bool):
     actual_result = check_power_of_2(value)
     if actual_result != expected_result:
-    	print(value) 
+        print(value)
     assert actual_result == expected_result
