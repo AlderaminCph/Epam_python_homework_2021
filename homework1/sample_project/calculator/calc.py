56148b803e858f43347ad120d2bb8a0b3325dde2
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-22 17:57:26 +0300

    file calc.py was edited with PEP8

--- homework1/sample_project/calculator/calc.py
+++ homework1/sample_project/calculator/calc.py
@@ -1,4 +1,5 @@
-'''task1
+"""
+task1
     1 please create your own git repository on github.
     2 (optional) setup pre-commit hook with black and isort formatting for the repo
     3 initialize .gitignore in the repository root (you can use this sample)
@@ -6,7 +7,12 @@
     5 then copy the sample_project into the directory.
     6 fix all bugs in the sample_project code
     7 write an extra test for each found bug
+"""
 
-'''
+
 def check_power_of_2(a: int) -> bool:
+    """
+    function that checks if the number is the power of 2
+    returns Bool
+    """
     return not (bool(a & (a - 1))) and a != 0
