8a581a1b80b1172265ade3c94980611aae6ef001
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-23 11:08:45 +0300

    task03 was done

---
+++ tests/homework1/test_task03.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+from typing import Tuple
+
+from homework1.task03 import find_maximum_and_minimum
+
+
+@pytest.mark.parametrize(
+    ["input_file", "expected_result"],
+    [
+        (os.path.join("tests/homework1/input_files", "input1.txt"), (0, 12)),
+        (os.path.join("tests/homework1/input_files", "input2.txt"), (-22, 100)),
+        (os.path.join("tests/homework1/input_files", "input3.txt"), (0, 0)),
+        (os.path.join("tests/homework1/input_files", "input4.txt"), (-1, 812)),
+        (os.path.join("tests/homework1/input_files", "input5.txt"), (-400, -1)),
+    ],
+)
+def test_find_maximum_and_minimum(input_file: str, expected_result: Tuple[int, int]):
+    our_result = find_maximum_and_minimum(input_file)
+
+    assert our_result == expected_result
