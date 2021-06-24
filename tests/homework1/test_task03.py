ba25f73c0f9862d72fc99485803650154e2690d5
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-24 14:50:00 +0300

    a variable for files directory was set in test_task03.py

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -5,15 +5,17 @@ import pytest
 
 from homework1.task03 import find_maximum_and_minimum
 
+path = "tests/homework1/input_files"
+
 
 @pytest.mark.parametrize(
     ["input_file", "expected_result"],
     [
-        (os.path.join("tests/homework1/input_files", "input1.txt"), (0, 12)),
-        (os.path.join("tests/homework1/input_files", "input2.txt"), (-22, 100)),
-        (os.path.join("tests/homework1/input_files", "input3.txt"), (0, 0)),
-        (os.path.join("tests/homework1/input_files", "input4.txt"), (-1, 812)),
-        (os.path.join("tests/homework1/input_files", "input5.txt"), (-400, -1)),
+        (os.path.join(path, "input1.txt"), (0, 12)),
+        (os.path.join(path, "input2.txt"), (-22, 100)),
+        (os.path.join(path, "input3.txt"), (0, 0)),
+        (os.path.join(path, "input4.txt"), (-1, 812)),
+        (os.path.join(path, "input5.txt"), (-400, -1)),
     ],
 )
 def test_find_maximum_and_minimum(input_file: str, expected_result: Tuple[int, int]):
