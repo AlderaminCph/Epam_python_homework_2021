7dd085b0cd243e471a6ea776b50a23ab584734bd
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-06-29 14:05:38 +0300

    test_task02.py was eddited, few tests were added

--- tests/homework1/test_task02.py
+++ tests/homework1/test_task02.py
@@ -36,8 +36,8 @@ from homework1.task02 import check_fibonacci
         ((55, 89, 144, 233, 377, 610), True),
         ((8, 13, 21), True),
         ((0, 0, 0, 0, 0), False),
-        ((2,1,3,4,7),False),
-        ((5,6,17,11,28),False),
+        ((2, 1, 3, 4, 7), False),
+        ((5, 6, 17, 11, 28), False),
     ],
 )
 def test_check_fibonacci(value: int, expected_result: bool):
