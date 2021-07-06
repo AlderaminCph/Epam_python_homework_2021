e31c448930f33910b77b66e6ad76cf857918b35e
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-06 20:36:00 +0300

    task4

--- homework2/task4.py
+++ homework2/task4.py
@@ -4,6 +4,7 @@ should return such a function, so the every call to initial one
 should be cached.
 def func(a, b):
     return (a ** b) ** 2
+
 cache_func = cache(func)
 some = 100, 200
 val_1 = cache_func(*some)
