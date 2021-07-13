ee04c693d1770af89e3e613b767109b79a317957
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-13 17:43:16 +0300

    task2

--- homework3/task2.py
+++ homework3/task2.py
@@ -8,6 +8,7 @@ import hashlib
 
 def slow_calculate(value):
     """Some weird voodoo magic calculations"""
+    
     time.sleep(random.randint(1,3))
     data = hashlib.md5(str(value).encode()).digest()
     return sum(struct.unpack('<' + 'B' * len(data), data))
