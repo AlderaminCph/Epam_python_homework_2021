abede695b26219a14b51456870b5dcc46389205d
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-13 22:19:18 +0300

    test_task4.py

--- tests/homework2/test_task4.py
+++ tests/homework2/test_task4.py
@@ -4,35 +4,22 @@ import pytest
 
 from homework2.task4 import cache
 
-vals = {(1, 2): 1, ("a", 2): 9}
+args = [(1, 2), ("a", 2), (1)]
+values = [1, 9, [3, 4, 5]]
+vals = dict(zip(args, values))
 
 
-def side_effect(*args):
-    return vals[args]
+def side_effect(arguments):
+    return vals[arguments]
 
 
-def test_cache_several_arguments():
+@pytest.mark.parametrize(
+    "arguments", [args[0], args[1], args[0], args[1], args[0], args[2], args[2]]
+)
+def test_cache_several_arguments(arguments):
+    print("Args=", arguments)
     mock_function = mock.Mock(side_effect=side_effect)
     input_function = mock_function
     mock_function = cache(mock_function)
-    mock_function(*list(vals.keys())[0])
-    mock_function(*list(vals.keys())[0])
-    assert input_function.call_count == 1
-
-
-def test_cache_different_types_arguments():
-    mock_function = mock.Mock(side_effect=side_effect)
-    input_function = mock_function
-    mock_function = cache(mock_function)
-    mock_function(*list(vals.keys())[1])
-    mock_function(*list(vals.keys())[1])
-    assert input_function.call_count == 1
-
-
-def test_cashe_without_arguments():
-    mock_function = mock.Mock()
-    input_function = mock_function
-    mock_function = cache(mock_function)
-    mock_function()
-    mock_function()
+    mock_function(arguments)
     assert input_function.call_count == 1
