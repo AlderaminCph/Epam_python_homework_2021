6173a7c20b4259fd0afe161aa0a3443bc244d6cd
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-14 11:39:24 +0300

    test_task4.py

--- tests/homework3/test_task4.py
+++ tests/homework3/test_task4.py
@@ -5,12 +5,7 @@ from homework3.task4 import is_armstrong
 
 @pytest.mark.parametrize(
     ["number", "expected_result"],
-    [
-        (9, True),
-        (10, False),
-        (153, True),
-        (0, True),
-    ],
+    [(9, True), (10, False), (153, True), (0, True), (371, True)],
 )
 def test_is_armstrong(number, expected_result):
     our_result = is_armstrong(number)
