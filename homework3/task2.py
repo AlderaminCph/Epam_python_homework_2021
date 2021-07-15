5fbe8dbd67bf579fe94a81205290d3d04a392900
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-15 17:10:41 +0300

    task2

--- homework3/task2.py
+++ homework3/task2.py
@@ -8,7 +8,7 @@ import hashlib
 
 def slow_calculate(value):
     """Some weird voodoo magic calculations"""
-    
+
     time.sleep(random.randint(1,3))
     data = hashlib.md5(str(value).encode()).digest()
     return sum(struct.unpack('<' + 'B' * len(data), data))
@@ -16,11 +16,11 @@ def slow_calculate(value):
 Calculate total sum of slow_calculate() of all numbers starting from 0 to 500. Calculation time should not take more than a minute. Use functional capabilities of multiprocessing module. You are not allowed to modify slow_calculate function.
 '''
 import hashlib
+import multiprocessing
 import random
 import struct
 import time
 from multiprocessing import Pool
-import multiprocessing
 
 
 def slow_calculate(value):
