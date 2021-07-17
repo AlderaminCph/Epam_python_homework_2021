7aad31d911c37b0a5cfee88ca96a2bf607fb8f84
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-17 17:03:27 +0300

    Task5 were edited

--- tests/homework2/test_task5.py
+++ tests/homework2/test_task5.py
@@ -15,6 +15,7 @@ from homework2.task5 import my_range_function
             ["g", "h", "i", "j", "k", "l", "m", "n", "o"],
         ),
         (string.ascii_lowercase, ("p", "g", -2), ["p", "n", "l", "j", "h"]),
+        ([1, 2, 3, 4, 5], (2, 5), [2, 3, 4]),
     ],
 )
 def test_my_range_function(iterable_values, arguments, expected_result):
@@ -25,3 +26,8 @@ def test_my_range_function(iterable_values, arguments, expected_result):
 def test_my_range_function_zero_step():
     with pytest.raises(ValueError):
         my_range_function(string.ascii_lowercase, "g", "p", 0)
+
+
+def test_my_range_beyond_limits():
+    with pytest.raises(ValueError):
+        my_range_function([1, 2, 3, 4, 5], (1, 6, 1))
