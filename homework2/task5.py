658b1496cb7a2dc06269e7dca4238e8328f8a279
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-17 17:02:35 +0300

    Test for exceeding limit case were written in task5

--- homework2/task5.py
+++ homework2/task5.py
@@ -11,7 +11,7 @@ assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', '
 import string
 
 
-def my_range_function(iterable_values, stop: str, start=None, step=1):
+def my_range_function(iterable_values, arg1: str, arg2=None, step=1):
     """Return an object that produces a sequence of iterable values from start (inclusive)
     to stop (exclusive) by step.
     range(i, j) produces i, i+1, i+2, ..., j-1.
@@ -19,22 +19,26 @@ def my_range_function(iterable_values, stop: str, start=None, step=1):
 
     Args:
         iterable_values:
-        stop: end value
-        start: first value
+        arg1: end value (if only two arguments) or first value (if 3 or more arguments)
+        arg2: first value (if step is negative) or end value (if step is positive)
         step: specifies the increment (positive) or decrement (negative)
     Returns:
         Sequence of iterable values from start to stop by step
     """
+    list_of_values = list(iterable_values)
     if step == 0:
         raise ValueError("step must be non-zero")
-    list_of_values = list(iterable_values)
-    if start is None:
-        result = list(iterable_values[0 : list_of_values.index(stop) : step])
+    if arg2 and arg2 not in list_of_values or arg1 not in list_of_values:
+        raise ValueError("arguments are beyond limits of iterable values")
+    if arg2:
+        start = arg1
+        stop = arg2
+        index_start = list_of_values.index(start)
     else:
-        start, stop = stop, start
-        result = list(
-            iterable_values[
-                list_of_values.index(start) : list_of_values.index(stop) : step
-            ]
-        )
+        index_start = 0
+        stop = arg1
+    index_stop = list_of_values.index(stop)
+    print("index_start = ", index_start)
+    print("index_stop = ", index_stop)
+    result = list(iterable_values[index_start:index_stop:step])
     return result
