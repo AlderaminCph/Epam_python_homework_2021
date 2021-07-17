4f42a9861e42db209dd7e0d08aa73bbcf5c2569a
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-17 16:27:44 +0300

    Tests became more complexin task4. Now it checks calling a function with different arguments

--- tests/homework2/test_task4.py
+++ tests/homework2/test_task4.py
@@ -4,29 +4,44 @@ import pytest
 
 from homework2.task4 import cache
 
-args = [(1, 2), ("a", 2), (1)]
-values = [1, 9, [3, 4, 5]]
-vals = dict(zip(args, values))
+
+def getArgStr(args, kwargs):
+    return str(args) + ", " + str(kwargs)
 
 
-def side_effect(arguments):
-    return vals[arguments]
+args = [(5, 6), (2, "someStr", [2, 3, 4])]
+kwargs = [{"k": 7}, {"qwert": [2, 3], "a": 9}]
+results = [3, 7]
+
+values = dict(zip((map(getArgStr, args, kwargs)), results))
+
+
+def side_effect(args, kwargs):
+    return values[getArgStr(args, kwargs)]
 
 
 @pytest.mark.parametrize(
-    "arguments", [args[0], args[1], args[0], args[1], args[0], args[2], args[2]]
+    "arguments, kwargs",
+    [
+        (args[0], kwargs[0]),
+        (args[1], kwargs[1]),
+        (args[0], kwargs[0]),
+        (args[1], kwargs[1]),
+        (args[0], kwargs[0]),
+    ],
 )
-def test_cache_several_arguments(arguments):
-    print("Args=", arguments)
+def test_cache_several_arguments(arguments, kwargs):
     mock_function = mock.Mock(side_effect=side_effect)
+    mock_function.__name__ = "foo"
     input_function = mock_function
     mock_function = cache(mock_function)
-    mock_function(arguments)
+    mock_function(arguments, kwargs)
     assert input_function.call_count == 1
 
 
 def test_cashe_without_arguments():
     mock_function = mock.Mock()
+    mock_function.__name__ = "foo"
     input_function = mock_function
     mock_function = cache(mock_function)
     mock_function()
