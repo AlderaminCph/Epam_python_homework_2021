0aa5c6964c3b01b1ef12744f4d84db91a6cd9f06
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-07-17 10:52:23 +0300

    test_task01 tests were added

--- tests/homework2/test_task01.py
+++ tests/homework2/test_task01.py
@@ -4,12 +4,14 @@ import pytest
 
 from homework2.task1 import *
 
+file = "homework2/data.txt"
+
 
 @pytest.mark.parametrize(
-    ["file", "expected_result"],
+    ["inputfile", "expected_result"],
     [
         (
-            os.path.join("homework2/data.txt"),
+            os.path.join(file),
             [
                 "unmißverständliche",
                 "Bevölkerungsabschub",
@@ -25,6 +27,26 @@ from homework2.task1 import *
         ),
     ],
 )
-def test_get_longest_diverse_words(file, expected_result: list):
-    actual_result = get_longest_diverse_words(file)
+def test_get_longest_diverse_words(inputfile, expected_result: list):
+    actual_result = get_longest_diverse_words(os.path.join(file))
     assert actual_result == expected_result
+
+
+def test_get_rarest_char():
+    actual_result = get_rarest_char(os.path.join(file))
+    assert actual_result == "›"
+
+
+def test_count_punctuation_chars():
+    actual_result = count_punctuation_chars(os.path.join(file))
+    assert actual_result == 5475
+
+
+def test_count_non_ascii_chars():
+    actual_result = count_non_ascii_chars(os.path.join(file))
+    assert actual_result == 2972
+
+
+def test_get_most_common_non_ascii_char():
+    actual_result = get_most_common_non_ascii_char(os.path.join(file))
+    assert actual_result == "ä"
