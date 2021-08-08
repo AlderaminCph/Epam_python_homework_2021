1ae28d6ddefd71d2248bb3724fee432486c63426
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-08-08 10:50:35 +0300

    task5.py

--- homework4/task5.py
+++ homework4/task5.py
@@ -12,8 +12,44 @@ Definition of done:
 * https://en.wikipedia.org/wiki/Fizz_buzz
 ** https://www.youtube.com/watch?v=NSzsYWckGd4
 """
-from typing import List, Generator
+from typing import Generator, List
 
 
 def fizzbuzz(n: int) -> Generator[str]:
-    pass
+    """Generator that takes a number N as an input
+    and returns a generator that yields N FizzBuzz numbers
+
+    Args:
+        n: number
+    Returns:
+        generator
+    """
+    generator = infinite_fizzbuzz()
+    for _ in range(n):
+        yield next(generator)
+
+
+def infinite_fizzbuzz():
+    i = 0
+    while True:
+        for element in generate_fizzbuzz_cycle(begin_elemnt=i * 15):
+            yield element
+        i += 1
+
+
+def generate_fizzbuzz_cycle(begin_elemnt):
+    yield begin_elemnt + 1
+    yield begin_elemnt + 2
+    yield "fizz"
+    yield begin_elemnt + 4
+    yield "buzz"
+    yield "fizz"
+    yield begin_elemnt + 7
+    yield begin_elemnt + 8
+    yield "fizz"
+    yield "buzz"
+    yield begin_elemnt + 11
+    yield "fizz"
+    yield begin_elemnt + 13
+    yield begin_elemnt + 14
+    yield "fizzbuzz"
