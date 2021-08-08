0e2047a716b166eeab99f6d149d57ca63f6718bb
Anastasia Sypkova <sypkova.anastasia@yandex.ru>
2021-08-08 10:24:06 +0300

    task4

--- homework4/task4.py
+++ homework4/task4.py
@@ -18,4 +18,38 @@ from typing import List
 
 
 def fizzbuzz(n: int) -> List[str]:
-    pass
+    """takes a number N as an input and returns N FizzBuzz numbers
+    >>> fizzbuzz(5)
+    ['1', '2', 'Fizz', '4', 'Buzz']
+    >>> fizzbuzz(0)
+    []
+    >>> fizzbuzz(-5)
+    []
+    >>> fizzbuzz(5.5)
+    Traceback (most recent call last):
+        ...
+    TypeError: 'float' object cannot be interpreted as an integer
+    >>> fizzbuzz(15)
+    ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'Fizz Buzz']
+
+    """
+    fizz_buzz = []
+    if not type(n) is int:
+        raise TypeError("'float' object cannot be interpreted as an integer")
+    else:
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                fizz_buzz.append("Fizz Buzz")
+            elif i % 3 == 0:
+                fizz_buzz.append("Fizz")
+            elif i % 5 == 0:
+                fizz_buzz.append("Buzz")
+            else:
+                fizz_buzz.append(str(i))
+        return fizz_buzz
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
